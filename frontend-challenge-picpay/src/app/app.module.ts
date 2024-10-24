565607b6a43d16578f7baf8dd9bd49dfefe945db
João Victor <joaodevcosta@gmail.com>
2024-10-23 23:07:35 -0300

    Adicionando primeng library

--- frontend-challenge-picpay/src/app/app.module.ts
+++ frontend-challenge-picpay/src/app/app.module.ts
@@ -1,29 +1,63 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, forwardRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PaymentListComponent } from './components/dashboard/payment-list/payment-list.component';
-import { PaymentFormComponent } from './components/dashboard/payment-form/payment-form.component';
-import { FormsModule } from '@angular/forms';
+import {TaskListComponent } from './components/dashboard/task-list/task-list.component';
+import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { PaidUnpaidPipe } from './components/shared/pipes/paid-unpaid.pipe';
+import { TaskFormComponent } from './components/dashboard/task-form/task-form.component';
+import { DialogModule } from 'primeng/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Adicione isso
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CheckboxModule } from 'primeng/checkbox';
+import { AccordionModule } from 'primeng/accordion';
+import { TaskFilterComponent } from './components/dashboard/task-filter/task-filter.component';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {ToolbarModule} from 'primeng/toolbar';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    PaymentListComponent,
-    PaymentFormComponent
+    TaskListComponent,
+    PaidUnpaidPipe,
+    TaskFormComponent,
+    TaskFilterComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
+    ButtonModule,
+    CheckboxModule,
+    DialogModule,
+    InputTextModule,
+    InputNumberModule,
     AppRoutingModule,
     FormsModule,
+    TableModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    RadioButtonModule,
+    ToolbarModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TaskFormComponent),
+    multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
